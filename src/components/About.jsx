d90aac56826ff2678eb3c62245a929d78a490149
Nina Adora <nirinakaes@gmail.com>
2026-05-11 11:29:50 +0300

    add About component structure with image and description

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,20 @@ export default function About (){
 
     return(
         <>
+        <aside>
+            <img
+             src="{image}"
+             alt="blog logo">
+            </img>
+
+            <p>
+                {about}
+            </p>
+
+
+
+
+        </aside>
         
         
         </>
