0d15105c5312ed7290894c516b355985caf5d739
Nina Adora <nirinakaes@gmail.com>
2026-05-11 11:39:17 +0300

    refactor Article component to include title, date, and preview rendering

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -5,6 +5,24 @@ export default function Article (){
 
     return(
         <>
+        <article>
+            <h3>
+                {title}
+            </h3>
+
+
+        </article>
+
+        <small>
+
+            {date}
+
+
+        </small>
+
+        <p>
+            {preview}
+        </p>
         
         
         </>
